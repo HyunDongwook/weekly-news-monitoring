e300a59773bb900c8addd103bce7f3bc44d6b112
Dongwook Hyun <hyundw@mnservice.co.kr>
2026-08-24 09:34:45 +0900

    Add 5 owner-submitted articles

--- scripts/update-news.mjs
+++ scripts/update-news.mjs
@@ -183,6 +183,8 @@ const SOURCE_NAME_MAP = {
     'realty.chosun.com': '땅집고',
     'newsway.co.kr': '뉴스웨이',
     'fntimes.com': '한국금융신문',
+    'nocutnews.co.kr': '노컷뉴스',
+    'ekn.kr': '에너지경제',
 };
 
 const EXCLUDE_URL_SUBSTR = ['msn.com'];
